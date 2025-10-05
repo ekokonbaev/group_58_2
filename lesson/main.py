64000ab014d93532462aa3e930c07167b0bd8e3c
ekokonbaev <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>
2025-10-05 17:06:18 +0600

    игнорь

--- lesson/main.py
+++ lesson/main.py
@@ -1,1 +1,12 @@
-print("Hi")
+import flet as ft 
+
+
+def main(page: ft.Page):
+    page.title = 'Моё первое приложение'
+
+    greeting_text = ft.Text("Hello world")
+
+    page.add(greeting_text)
+
+
+ft.app(target=main)

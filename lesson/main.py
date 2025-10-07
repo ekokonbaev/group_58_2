16436b18d19cee96ae1318b32a0eeb62adebb962
ekokonbaev <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>
2025-10-07 20:42:26 +0600

    edit3

--- lesson/main.py
+++ lesson/main.py
@@ -6,7 +6,20 @@ def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.LIGHT
     greeting_text = ft.Text("Hello world")
     name_input = ft.TextField(label="Ваше имя: ")
-    name_button = ft.ElevatedButton(text="SEND" , icon=ft.Icons.SEND)
+
+    def on_button_click(_):
+        name = name_input.value.strip()
+        print(name)
+        page.update()
+
+        if name:
+            greeting_text.value = f"Hello"
+        else:
+            print("Please enter your name")
+        page.update()
+
+
+    name_button = ft.ElevatedButton(text="SEND" , icon=ft.Icons.SEND, on_click=on_button_click)
     #name_text_button = ft.TextButton(text="Text Button")
     #name_icon_buttin = ft.IconButton(icon=ft.Icons.Send)
     page.add(greeting_text , name_input, name_button)

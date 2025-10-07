8370488847f34f42e4f8de3f1c24ed28f5e066b8
ekokonbaev <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>
2025-10-07 19:29:35 +0600

    edit

--- lesson/main.py
+++ lesson/main.py
@@ -3,8 +3,9 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'Моё первое приложение'
-
+    page.theme_mode = ft.ThemeMode.SYSTEM
     greeting_text = ft.Text("Hello world")
+    name_input = ft.TextField(label="Ваше имя: ")
 
     page.add(greeting_text)
 

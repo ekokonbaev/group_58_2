292ec2fc7889e19d560682bbe25c14a23c817908
ekokonbaev <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>
2025-10-07 19:48:35 +0600

    edit2

--- lesson/main.py
+++ lesson/main.py
@@ -3,11 +3,13 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'Моё первое приложение'
-    page.theme_mode = ft.ThemeMode.SYSTEM
+    page.theme_mode = ft.ThemeMode.LIGHT
     greeting_text = ft.Text("Hello world")
     name_input = ft.TextField(label="Ваше имя: ")
-
-    page.add(greeting_text)
+    name_button = ft.ElevatedButton(text="SEND" , icon=ft.Icons.SEND)
+    #name_text_button = ft.TextButton(text="Text Button")
+    #name_icon_buttin = ft.IconButton(icon=ft.Icons.Send)
+    page.add(greeting_text , name_input, name_button)
 
 
 ft.app(target=main)

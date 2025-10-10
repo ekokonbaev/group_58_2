7e65c3f20bffd1dda8178ee7cbe2f1ae9907f143
ekokonbaev <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>
2025-10-10 20:00:18 +0600

    update main

--- lesson/main.py
+++ lesson/main.py
@@ -1,32 +1,32 @@
-import flet as ft 
-
+import flet as ft
+from datetime import datetime
 
 def main(page: ft.Page):
     page.title = 'Моё первое приложение'
     page.theme_mode = ft.ThemeMode.LIGHT
     greeting_text = ft.Text("Hello world")
 
-    greeting_text = []
+    history = []
     history_text = ft.Text('История сообщений: ')
 
     def on_button_click(_):
         name = name_input.value.strip()
         print(name)
-        page.update()
 
         if name:
-            greeting_text.value = f"Hello"
+            greeting_text.value = f"Hello {name}"
+            name_input.value = ""
+            history.append(f"{name} вошёл в чат в {datetime.now().strftime('%H:%M:%S')}")
+            timestamp = datetime.now().strftime('%H:%M:%S')
+            history_text.value = 'История сообщений:     ' + '\n'.join(history)
         else:
             print("Please enter your name")
         page.update()
 
-    
-    name_input = ft.TextField(label="Ваше имя: " , on_submit=on_button_click)
+    name_input = ft.TextField(label="Ваше имя: ", on_submit=on_button_click)
+    name_button = ft.ElevatedButton(text="SEND", icon=ft.Icons.SEND, on_click=on_button_click)
 
-    name_button = ft.ElevatedButton(text="SEND" , icon=ft.Icons.SEND, on_click=on_button_click)
-    #name_text_button = ft.TextButton(text="Text Button")
-    #name_icon_buttin = ft.IconButton(icon=ft.Icons.Send)
-    page.add(greeting_text , name_input, name_button, history_text)
+    page.add(greeting_text, name_input, name_button, history_text)
 
 
 ft.app(target=main)

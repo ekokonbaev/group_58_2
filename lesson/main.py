87b3d1a41a6eae6199a794ba591b09024d889e90
ekokonbaev <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>
2025-10-10 19:24:09 +0600

    what is

--- lesson/main.py
+++ lesson/main.py
@@ -5,7 +5,9 @@ def main(page: ft.Page):
     page.title = 'Моё первое приложение'
     page.theme_mode = ft.ThemeMode.LIGHT
     greeting_text = ft.Text("Hello world")
-    name_input = ft.TextField(label="Ваше имя: ")
+
+    greeting_text = []
+    history_text = ft.Text('История сообщений: ')
 
     def on_button_click(_):
         name = name_input.value.strip()
@@ -18,11 +20,13 @@ def main(page: ft.Page):
             print("Please enter your name")
         page.update()
 
+    
+    name_input = ft.TextField(label="Ваше имя: " , on_submit=on_button_click)
 
     name_button = ft.ElevatedButton(text="SEND" , icon=ft.Icons.SEND, on_click=on_button_click)
     #name_text_button = ft.TextButton(text="Text Button")
     #name_icon_buttin = ft.IconButton(icon=ft.Icons.Send)
-    page.add(greeting_text , name_input, name_button)
+    page.add(greeting_text , name_input, name_button, history_text)
 
 
 ft.app(target=main)
